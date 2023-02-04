a215a0e92e3fb92147751506c2d0d821dc136708
Manoel C <manoelclemente9@outlook.com>
2023-02-04 17:47:04 -0300

    add command patern

---
+++ src/behaviorals/TestesPedido.java
@@ -0,0 +1,15 @@
+package behaviorals;
+
+import behaviorals.Orcamento.Orcamento;
+import behaviorals.pedido.Pedido;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+public class TestesPedido {
+
+    public static void main(String[] args) {
+
+
+    }
+}
---
+++ src/behaviorals/pedido/GeraPedidoHandler.java
@@ -0,0 +1,18 @@
+package behaviorals.pedido;
+
+import behaviorals.Orcamento.Orcamento;
+
+import java.time.LocalDateTime;
+
+public class GeraPedidoHandler {
+
+    public void execute(GeraPedido dados){
+        Orcamento orcamento = new Orcamento(dados.getValorOrcamento(), dados.getQuantidadeItens());
+
+        Pedido pedido = new Pedido(dados.getCliente(), LocalDateTime.now(),orcamento);
+
+        System.out.println("Salvar pedido no banco de dados");
+        System.out.println("Enviar email com dados do novo pedido");
+    }
+
+}
